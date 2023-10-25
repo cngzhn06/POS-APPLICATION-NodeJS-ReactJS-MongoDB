b8d7353f45ffb8b49e4c4c7e85906771471cdaa3
Cengizhan <cngzhnc06@gmail.com>
2023-10-25 21:05:24 +0300

    update

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 import { Provider } from "react-redux";
-import store from "./redux/store.js"; // `store.js` dosyasından `store` değişkenini doğru şekilde import ediyoruz
+import store from "./redux/store.jsx"; // `store.js` dosyasından `store` değişkenini doğru şekilde import ediyoruz
 
 ReactDOM.createRoot(document.getElementById("root")).render(
     <Provider store={store}>
